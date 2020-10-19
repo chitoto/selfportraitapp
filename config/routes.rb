e30bb72953728cafb0bf6a6eabe087a35b9b3551
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-10-19 15:08:14 +0900

    add_favorites_function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'users#new'
   resources :pictures do
     collection do
       post :confirm
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources :favorites, only: [:create, :destroy]
 end
