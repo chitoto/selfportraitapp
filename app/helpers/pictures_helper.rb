db2124933d142e41ed997de3fa543fb3267041d4
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-10-26 09:12:37 +0900

    fixed:PicturesHelper

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -2,7 +2,7 @@ module PicturesHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
       confirm_pictures_path
-    elsif
+    elsif action_name == 'edit'
       pictures_path
     end
   end
