ac5749a28e9ed07194e46d0b7649edf4ce87388f
Yumiko Kamiyama <tototina.ruru@gmail.com>
2020-10-26 10:35:59 +0900

    fixed:error_edit

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -2,8 +2,6 @@ module PicturesHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'create'
       confirm_pictures_path
-    elsif action_name == 'edit'
-      pictures_path
     end
   end
 end
